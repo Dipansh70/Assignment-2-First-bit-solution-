317a14903b40082f75721f3f1fc15ea336227c21
Dipanshu Sharma <dipanshusharma7015@gmail.com>
2025-07-31 01:04:07 +0530

    Question 1. = Answer

---
+++ nested1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main() {
+	int a = 2;
+	int b = 4;
+	
+	printf("the value of %d\n", a+b);
+	
+	printf("the value of %d\n", a-b);
+	
+	printf("the value of %d\n", a*b);
+	
+	printf("the value of %d\n", a/b);
+	
+	printf("the value of %d\n", a%b);
+	
+}
